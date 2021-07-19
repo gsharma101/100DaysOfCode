429bcc7e4959d7b3bbefd6ee1a3e453dd6f0232f
Gaurav Sharma <sharmag8121@gmail.com>
2021-07-19 20:06:07 +0530

    Day one complete

---
+++ Day1/hello_world.py
@@ -0,0 +1,12 @@
+print("Hello World")
+#! Coding Challage 1
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+#! using \n to print in new line
+print("Hello World\nHello World")
+#! Using input function
+print("Hello" + input("What's your name"))
+#! Coding Challange 2
+name = input("What is your name ") 
+print(len(name))
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # 100DaysOfCode
-This repository consist of 100 days of consistent coding started at 17July 10:48pm
+This repository consist of 100 days of consistent coding started at 17 July 10:48pm
